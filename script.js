a090be6dacd425880d7a5ebee51b91f960355390
mops.dev <ilendel869@gmail.com>
2026-03-07 22:47:20 +0200

    Add event listeners to change background color

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const btn = document.getElementById("btn");
 
+btn.addEventListener("click", function () {
+    document.body.style.background = "#181717";
+});
+
+
+const btnnew = document.getElementById("btnnew");
+
+btn.addEventListener("click", function () {
+    document.body.style.background = "#0A0A0F";
+});
