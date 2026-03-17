dbb998ec24facd38ed8f35b4d00685ad2ed5e390
mops.dev <ilendel869@gmail.com>
2026-03-17 17:29:52 +0200

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,1 +1,11 @@
+const btn = document.getElementById("btn");
 
+btn.addEventListener("click", function () {
+    document.body.style.background = "#181717";
+});
+
+const btnx = document.getElementById("btnx")
+
+btnx.addEventListener("click", function () {
+    document.body.style.background = "#0A0A0F";
+})
